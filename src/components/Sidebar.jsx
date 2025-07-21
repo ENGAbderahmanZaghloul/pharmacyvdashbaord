8268c24b3e520b9f037d5107f2a97fae0cccf0d7
ENGAbderahmanZaghloul <114342047+ENGAbderahmanZaghloul@users.noreply.github.com>
2025-07-21 18:51:59 +0300

    add toggle btn to sidebar open and close it

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,7 @@
+// import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { TbSquareRoundedArrowRight } from "react-icons/tb";
+
 const sidebarLi = [
   {
     patheName: "/overview",
@@ -33,7 +36,6 @@ const sidebarLi = [
 ];
 
 const sidebarLiHelp = [
-
   {
     patheName: "/help",
     icon: "/images/help.png",
@@ -50,16 +52,37 @@ const sidebarLiHelp = [
     title: " Log Out",
   },
 ];
-const Sidebar = () => {
+const Sidebar = ({ openSideBar, setOpenSideBar }) => {
+  // const [openSideBar, setOpenSideBar] = useState(true);
   const location = useLocation();
   const { pathname } = location;
+
+  // handler
+  const SideBarToggle = () => {
+    setOpenSideBar(!openSideBar);
+  };
   return (
-    <div className="w-[200px] h-fit bg-[#000E2E]  text-white font-normal rounded-r-3xl px-5 py-10 absolute">
+    <div
+      className={`w-[200px] h-fit bg-[#000E2E]  text-white font-normal rounded-r-3xl px-5 py-10 fixed transition-all duration-500 ease-in-out  ${
+        openSideBar ? "translate-x-0" : "-translate-x-36"
+      } `}
+    >
       <div className="flex justify-between">
         <div className="flex gap-2">
           <img src="/public/images/Logo.png" alt="" />
           <h1>Pharmy</h1>
         </div>
+        <button onClick={SideBarToggle}>
+          {openSideBar ? (
+            <img
+              src="/public/images/arrow-square-left.png"
+              className="h-6"
+              alt=""
+            />
+          ) : (
+            <TbSquareRoundedArrowRight className="h-6 w-6" />
+          )}
+        </button>
       </div>
       <div className="h-[2px] bg-white my-10"></div>
       <ul className="space-y-4">
@@ -101,128 +124,6 @@ const Sidebar = () => {
             </li>
           ))}
       </ul>
-      {/* <ul className="space-y-4">
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/overview"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/lists.png" alt="" />
-          <Link to="/overview" className="hover:text-blue-300">
-            Overview
-          </Link>
-        </li>
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/products.png" alt="" />
-          <Link to="/" className="hover:text-blue-300">
-            Products
-          </Link>
-        </li>
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/orders"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/cart.png" alt="" />
-          <Link to="/orders" className="hover:text-blue-300">
-            Orders
-          </Link>
-        </li>
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/sales"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/sales.png" alt="" />
-          <Link to="/sales" className="hover:text-blue-300">
-            Sales
-          </Link>
-        </li>
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/customers"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/customers.png" alt="" />
-          <Link to="/customers" className="hover:text-blue-300">
-            Customers
-          </Link>
-        </li>
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/payments"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/dollar.png" alt="" />
-          <Link to="/payments" className="hover:text-blue-300">
-            Payments
-          </Link>
-        </li>
-
-        <div className="h-[2px] bg-white !my-10"></div>
-
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/help"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/help.png" alt="" />
-          <Link to="/help" className="hover:text-blue-300">
-            Help & Support
-          </Link>
-        </li>
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/settings"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/setting.png" alt="" />
-          <Link to="/settings" className="hover:text-blue-300">
-            Settings
-          </Link>
-        </li>
-        <li
-          className={`flex gap-1 hover:p-2 cursor-pointer   hover:bg-[#2563EB] hover:rounded-full
-    ${
-      pathname === "/logout"
-        ? "bg-[#2563EB] text-white p-2 rounded-full"
-        : "hover:bg-[#2563EB] hover:text-white"
-    }`}
-        >
-          <img src="/public/images/logout.png" alt="" />
-          <Link to="/logout" className="hover:text-blue-300">
-            Log Out
-          </Link>
-        </li>
-      </ul> */}
     </div>
   );
 };
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -14,12 +14,16 @@
 // export default RootLayout;
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import { useState } from "react";
 
 const Layout = () => {
+  const [openSideBar, setOpenSideBar] = useState(true);
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <main className="flex-1 bg-gray-100 ">
+    <div className="flex h-screen transition-all duration-500 ease-in-out">
+      <div >
+        <Sidebar openSideBar={openSideBar} setOpenSideBar={setOpenSideBar} />
+      </div>
+      <main className="flex-1 bg-gray-100 transition-all duration-500 ease-in-out overflow-hidden">
         <Outlet />
       </main>
     </div>
